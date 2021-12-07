05ae36013adaac1ede4f26227c99753997b50208
Calytic <11840727+Calytic@users.noreply.github.com>
2021-12-07 08:39:31 -0500

    Update MyTestPlugin.cs

--- MyTestPlugin.cs
+++ MyTestPlugin.cs
@@ -1,6 +1,6 @@
 namespace uMod.Plugins
 {
-    [Info("My Test Plugin", "uMod", "1.0.11")]
+    [Info("My Test Plugin", "uMod", "1.0.12")]
     [Description("Test Plugin")]
     public class MyTestPlugin : Plugin
     {
